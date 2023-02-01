89b32911a7ba2a43ecd0b98b5621e70480cc09f1
Salem-Tarek <salemtarek224@gmail.com>
2023-02-01 03:19:47 +0200

    Deploying Dist Folder

---
+++ precache-manifest.0be50427f2c848b727b56ed497caaea8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "faead63bd7f7d457446c",
+    "url": "/emotion-task/css/about.d859dd76.css"
+  },
+  {
+    "revision": "f8da6958dc347c2034f3",
+    "url": "/emotion-task/css/app.0ffa48a5.css"
+  },
+  {
+    "revision": "03352f4332ba56d50ec1",
+    "url": "/emotion-task/css/chunk-vendors.46edd95b.css"
+  },
+  {
+    "revision": "63a7d78d42c33b94fc7b957524795cac",
+    "url": "/emotion-task/img/logo.63a7d78d.svg"
+  },
+  {
+    "revision": "948f39d3d5d81657e67a793c016b3290",
+    "url": "/emotion-task/index.html"
+  },
+  {
+    "revision": "faead63bd7f7d457446c",
+    "url": "/emotion-task/js/about.64ec844e.js"
+  },
+  {
+    "revision": "f8da6958dc347c2034f3",
+    "url": "/emotion-task/js/app.69f0cf25.js"
+  },
+  {
+    "revision": "03352f4332ba56d50ec1",
+    "url": "/emotion-task/js/chunk-vendors.07b30787.js"
+  },
+  {
+    "revision": "adfe9a92f47a6e040ef14c5fe2fcf990",
+    "url": "/emotion-task/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/emotion-task/robots.txt"
+  }
+]);
